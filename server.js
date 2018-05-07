63bfec5fca3d0f181947b76f581f8bb76934f2a1
Joshua Arnett <Josharnett850@gmail.com>
2018-05-07 08:15:26 -0400

    requiring server packages

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const router = require("./controllers/burgers_controllers.js");
+
+app.use("/", router);
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
