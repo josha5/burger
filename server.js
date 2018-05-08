483b850c89493f4df8e9b6a560ed3ba816e89f56
Joshua Arnett <Josharnett850@gmail.com>
2018-05-07 21:26:46 -0400

    changing port back

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const router = require("./controllers/burgers_controller.js");
 
 app.use("/", router);
 
-server.listen(process.env.PORT || port, function() {
-    console.log("Server is listening on port" + PORT);
-})
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
