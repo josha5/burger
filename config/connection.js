7021dbd1a558606e5c93c2ef4485e20abe5842aa
Joshua Arnett <Josharnett850@gmail.com>
2018-05-07 22:19:37 -0400

    changing connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,16 @@
 const mysql = require("mysql");
 
 // create the connection information for the sql database
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "password",
-  database: "burgers_db"
-});
-var jawsConnection = mysql.createConnection(process.env.JAWSDB_URL);
-
-jawsConnection.connect();
-
-jawsConnection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password', 
+    database : 'burgers_db' 
+  });
+}
 
 connection.connect(function(err) {
   if (err) throw err;
